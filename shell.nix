03c07dd768db571e6a960bad292b87af3b9669e6
Jean-Philippe Bernardy <jeanphilippe.bernardy@gmail.com>
2016-11-18 15:31:46 +0100

    fix nixpkgs version

--- shell.nix
+++ shell.nix
@@ -1,5 +1,12 @@
 { nixpkgs ? import <nixpkgs> {}, compiler ? "ghc801" }:
-with (import <nixpkgs> {}).pkgs;
+let nixpkgs_source = nixpkgs.fetchFromGitHub {
+      owner = "NixOS";
+      repo = "nixpkgs";
+      rev = "8ef3eaeb4e531929ec29a880cb4c67f790e5eb70";
+      sha256 = "1v9lgk3j394i91qz1h7cv6mbg6xkdllfccc902ydb1gvp6bzmh6z";
+    };
+    nixpkgs' = (import nixpkgs_source){};
+in with nixpkgs'.pkgs;
 let hp = haskell.packages.${compiler}.override{
     overrides = self: super: {
       };};
